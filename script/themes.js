fca69a283e6c9570a95be0a9ebc67f54fc038ea5
ammar bannan <ammarabannan@gmail.com>
2023-07-31 00:56:08 +0300

    local storage

--- script/themes.js
+++ script/themes.js
@@ -1,10 +1,29 @@
-let mode =false
+let mode
+
+
+if ( localStorage.getItem("mode") !== null) {
+    mode=localStorage.getItem("mode");
+    
+}else{
+    localStorage.setItem("mode", true);
+}
+
+mode=localStorage.getItem("mode")
 
 var link = document.createElement( "link" );
-link.href="./css/themes/light.css";
+if(mode=="true"){
+    link.href="./css/themes/dark.css";
+    
+}else{
+    link.href="./css/themes/light.css";
+    document.querySelector('.switch input').checked = false;
+}
 link.type = "text/css";
 link.rel = "stylesheet";
 link.media = "screen,print";
+
+
+console.log("here:",link.href)
 
 document.getElementsByTagName( "head" )[0].appendChild( link );
 
@@ -12,13 +31,14 @@ document.querySelector(".switch input").addEventListener("click",()=>{
     if(mode){
         mode=false;
         document.querySelector("link[href='./css/themes/dark.css']").href = "./css/themes/light.css";
+        document.querySelector('.switch input').checked = false;
     }else{
         mode=true;
         document.querySelector("link[href='./css/themes/light.css']").href = "./css/themes/dark.css";
+        document.querySelector('.switch input').checked = true;
     }
   
-   
-
+    localStorage.setItem("mode", mode);
     
 })
 
